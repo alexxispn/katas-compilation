98842d0598426497964f048e24a514b06c6f62e6
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-22 01:55:44 +0100

    add getVowelCount resolved by filter method

--- src/E02_vowel_count/exercise.ts
+++ src/E02_vowel_count/exercise.ts
@@ -1,5 +1,14 @@
 export function getVowelCount(text: string): number {
     const vowels = 'aeiouáéíóúü';
+    return text.toLowerCase()
+        .split('')
+        .filter(letter => vowels.includes(letter))
+        .length;
+}
+
+// Using reduce mehotd
+export function getVowelCountAlt(text: string): number {
+    const vowels = 'aeiouáéíóúü';
     const charsArray = text.toLowerCase()
         .split('');
     return charsArray.reduce((count, char) => (vowels.includes(char) ? count + 1 : count), 0);
