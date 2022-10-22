b12eaa6b50fa76b9a77cba8fe8ded74d49af74ed
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-22 03:01:28 +0100

    refactor getMousePosition using findIndex

--- src/E08_cheese_hunt/exercise.ts
+++ src/E08_cheese_hunt/exercise.ts
@@ -1,44 +1,33 @@
 enum DirectionEnum {
-	up = 'up',
-	down = 'down',
-	left = 'left',
-	right = 'right',
+    up = 'up',
+    down = 'down',
+    left = 'left',
+    right = 'right',
 }
 
 type DirectionType = keyof typeof DirectionEnum;
 const MOUSE_SYMBOL = 'm';
 const CHEESE_SYMBOL = '*';
+
 const getMousePosition = (room: string[][]): any => {
-	for (let y = 0; y < room.length; y++) {
-		for (let x = 0; x < room[y].length; x++) {
-			if (room[y][x] === MOUSE_SYMBOL) {
-				return { x, y };
-			}
-		}
-	}
+    const y = room.findIndex(row => row.includes(MOUSE_SYMBOL));
+    if (y === -1) return null;
+    const x = room[y].findIndex(cell => cell === MOUSE_SYMBOL);
+    return { x, y };
 };
 
-export function canMouseEat(direction: DirectionType, game: string[][]): boolean {
-	const mousePosition = getMousePosition(game);
-	if (!mousePosition) return false;
-	let { x, y } = mousePosition;
-	const cantEatUp = y === 0;
-	const cantEatDown = y === game.length - 1;
-	const cantEatLeft = x === 0;
-	const cantEatRight = x === game[y].length - 1;
-
-	if (direction === DirectionEnum.up) {
-		if (cantEatUp) return false;
-		y--;
-	} else if (direction === DirectionEnum.down) {
-		if (cantEatDown) return false;
-		y++;
-	} else if (direction === DirectionEnum.left) {
-		if (cantEatLeft) return false;
-		x--;
-	} else if (direction === DirectionEnum.right) {
-		if (cantEatRight) return false;
-		x++;
-	}
-	return game[y][x] === CHEESE_SYMBOL;
+export function canMouseEat(direction: DirectionType, room: string[][]): boolean {
+    const mousePosition = getMousePosition(room);
+    if (!mousePosition) return false;
+    let { x, y } = mousePosition;
+    if (direction === DirectionEnum.up) {
+        y--;
+    } else if (direction === DirectionEnum.down) {
+        y++;
+    } else if (direction === DirectionEnum.left) {
+        x--;
+    } else if (direction === DirectionEnum.right) {
+        x++;
+    }
+    return room[y]?.[x] === CHEESE_SYMBOL;
 }
