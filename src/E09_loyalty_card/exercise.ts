91f220c1f055a908e9c6b6f62119fc1ac78863fe
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-22 03:14:27 +0100

    refactor shouldBuyLoyaltyCard

--- src/E09_loyalty_card/exercise.ts
+++ src/E09_loyalty_card/exercise.ts
@@ -4,15 +4,12 @@ const discount = 0.75;
 const getNormalPrice = (numOfTickets: number): number => normalPrice * numOfTickets;
 const getLoyaltyPrice = (numOfTickets: number): number => {
     let price = loyaltyPrice;
-    for (let i = 0; i < numOfTickets; i++) {
-        price += normalPrice * discount ** i;
+    for (let ticket = 1; ticket <= numOfTickets; ticket += 1) {
+        price += normalPrice * discount ** ticket;
     }
     return price;
 };
 
 export function shouldBuyLoyaltyCard(numOfTickets: number): boolean {
-    if (numOfTickets < 1) return false;
-    const finalNormalPrice = getNormalPrice(numOfTickets);
-    const finalLoyaltyPrice = getLoyaltyPrice(numOfTickets);
-    return finalNormalPrice > finalLoyaltyPrice;
+    return getLoyaltyPrice(numOfTickets) < getNormalPrice(numOfTickets);
 }
